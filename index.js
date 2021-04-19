4a236b63d6c11790d57bb364de977d4570fd8c5a
zhangyuang <leonin0829@gmail.com>
2021-04-19 19:38:18 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,16 @@
+module.export = function markdownRawPlugin (options) {
+  return {
+    name: 'vite-raw-plugin',
+    transform (code, id) {
+      if (options.fileRegex.test(id)) {
+        const json = JSON.stringify(code)
+          .replace(/\u2028/g, '\\u2028')
+          .replace(/\u2029/g, '\\u2029')
+
+        return {
+          code: `export default ${json}`
+        }
+      }
+    }
+  }
+}
